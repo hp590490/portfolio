175e25288ead43a560278680c4835e136aa98895
Hugo PLASTRE <hugo.plastre@gmail.com>
2025-01-05 11:15:27 +0100

    done

--- portfolio-react/src/components/C-card.js
+++ portfolio-react/src/components/C-card.js
@@ -1,8 +1,14 @@
-import React from "react";
-const Ccard = ({ images, title }) => {
+import React, { useState } from "react";
+const Ccard = ({ images, title, competences }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div id="cards-container">
-      <div id="card">
+    <div
+      id="cards-container"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div id="card" className={isHovered ? "hovered" : ""}>
         <div className="front">
           <div className="front-img">
             {images.map((imgSrc, index) => (
@@ -14,7 +20,15 @@ const Ccard = ({ images, title }) => {
             <p>Et si tu survolais la carte ?</p>
           </div>
         </div>
-        <div className="back"></div>
+        <div className="back">
+          <h4>{title}</h4>
+
+          <ul>
+            {competences.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
